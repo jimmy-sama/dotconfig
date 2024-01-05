12e93e9f4043cb3a28fa456d94f4742b58f5e7d1
jimmy-sama <lungmuss@proton.me>
2024-01-05 11:59:28 +0100

    zshrc clean up and organization

---
+++ zsh/.oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,53 @@
+# To temporarily bypass an alias, we precede the command with a \
+
+alias vim='nvim'
+alias vi='nvim'
+
+alias cat='batcat'
+
+# Alias's for SSH
+# alias SERVERNAME='ssh YOURWEBSITE.com -l USERNAME -p PORTNUMBERHERE'
+alias phanpy='ssh phanpy.logodata.intern -l admin'
+
+# Alias's to modified commands
+alias mkdir='mkdir -p'
+alias ping='ping -c 10'
+
+# Remove a directory and all files
+alias rmd='/bin/rm  --recursive --force --verbose '
+
+# Alias's for directory listing commands
+alias ls='ls -lAFh --color=always' # add colors and file type extensions
+
+# Search running processes
+alias p="ps aux | grep "
+alias topcpu="/bin/ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10"
+
+# Show open ports
+alias openports='netstat -nape --inet'
+
+# Alias's for safe and forced reboots
+alias rebootsafe='sudo shutdown -r now'
+alias rebootforce='sudo shutdown -r -n now'
+
+# Alias's to show disk space and space used in a folder
+alias diskspace="du -S | sort -n -r |more"
+alias folders='du -h --max-depth=1'
+alias folderssort='find . -maxdepth 1 -type d -print0 | xargs -0 du -sk | sort -rn'
+alias tree='tree -CAhF --dirsfirst'
+alias treed='tree -CAFd'
+alias mountedinfo='df -hT'
+
+# Alias's for archives
+alias mktar='tar -cvf'
+alias mkbz2='tar -cvjf'
+alias mkgz='tar -cvzf'
+alias untar='tar -xvf'
+alias unbz2='tar -xvjf'
+alias ungz='tar -xvzf'
+
+# Show all logs in /var/log
+alias logs="sudo find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -f1 | sed -e's/:$//g' | grep -v '[0-9]$' | xargs tail -f"
+
+alias clickpaste='sleep 3; xdotool type "$(xclip -o -selection clipboard)"'
+
