4c79c5c1ebf936e1277ef408a2b4a9fde8513188
jimmy-sama <lungmuss@proton.me>
2025-01-08 07:40:18 +0100

    add terminal commands

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -16,9 +16,37 @@ vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
+})
+
+local job_id = 0
+vim.keymap.set("n", "<space>to", function()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 10)
+
+    job_id = vim.bo.channel
+end)
+
+local current_command = ""
+
+vim.keymap.set("n", "<space>tr", function()
+    if current_command == "" then
+        current_command = vim.fn.input("Command: ")
+    end
+
+    vim.fn.chansend(job_id, { current_command .. "\r\n" })
+end)
