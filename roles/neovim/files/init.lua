0093e3d78c9001547b94a712cc0ebee0e4bb6cea
Tim Lungmuss <lungmuss@proton.me>
2025-06-19 20:22:10 +0200

    starting with default obsidian.nvim conf

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -6,6 +6,7 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.conceallevel = 1
 
 local set = vim.keymap.set
 
@@ -24,43 +25,43 @@ set("n", "<M-k>", "<cmd>cprev<CR>")
 
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking text',
-    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-    callback = function()
-        vim.opt.number = false
-        vim.opt.relativenumber = false
-    end,
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
 })
 
 local job_id = 0
 set("n", "<space>to", function()
-    vim.cmd.new()
-    vim.cmd.term()
-    vim.cmd.wincmd("J")
-    vim.api.nvim_win_set_height(0, 10)
+  vim.cmd.new()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 10)
 
-    job_id = vim.bo.channel
+  job_id = vim.bo.channel
 end)
 
 set("n", "<space>tc", function()
-    -- go build, go test ./someFile
-    vim.fn.chansend(job_id, { "ls -lA\r\n" })
+  -- go build, go test ./someFile
+  vim.fn.chansend(job_id, { "ls -lA\r\n" })
 end)
 
 local current_command = ""
 set("n", "<space>tr", function()
-    if current_command == "" then
-        current_command = vim.fn.input("Command: ")
-    end
+  if current_command == "" then
+    current_command = vim.fn.input("Command: ")
+  end
 
-    vim.fn.chansend(job_id, { current_command .. "\r\n" })
+  vim.fn.chansend(job_id, { current_command .. "\r\n" })
 end)
 
 set("n", "-", "<cmd>Oil<CR>")
