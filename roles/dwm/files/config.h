b591f33cd951ceedb89ac6448cce6ff590e6b309
jimmy-sama <lungmuss@proton.me>
2025-08-17 11:09:41 +0200

    add sound control on the corresponding keys

--- roles/dwm/files/config.h
+++ roles/dwm/files/config.h
@@ -1,7 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-#include <X11/XF86keysym.h>
-
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -22,10 +20,6 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
-/* brightness control keys */
-static const char *brupcmd[] = { "brightnessctl", "set", "10%+", NULL };
-static const char *brdowncmd[] = { "brightnessctl", "set", "10%-", NULL };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -61,16 +55,28 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
+// commands 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "ghostty", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
+// brightness control keys 
+static const char *brupcmd[] = { "brightnessctl", "set", "10%+", NULL };
+static const char *brdowncmd[] = { "brightnessctl", "set", "10%-", NULL };
+// volume control for pipewire
+static const char *upvol[]      = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%+",      NULL };
+static const char *downvol[]    = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%-",      NULL };
+static const char *mutevol[]    = { "/usr/bin/wpctl",   "set-mute",   "@DEFAULT_AUDIO_SINK@",      "toggle",   NULL };
+
+#include <X11/XF86keysym.h>
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
         { 0, 				XF86XK_MonBrightnessUp,  	spawn,          {.v = brupcmd} },
 	{ 0, 				XF86XK_MonBrightnessDown, 	spawn,          {.v = brdowncmd} },
+	{ 0,                       	XF86XK_AudioRaiseVolume, 	spawn, 		{.v = upvol   } },
+	{ 0,                       	XF86XK_AudioLowerVolume, 	spawn, 		{.v = downvol } },
+	{ 0,                       	XF86XK_AudioMute, 		spawn, 		{.v = mutevol } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
