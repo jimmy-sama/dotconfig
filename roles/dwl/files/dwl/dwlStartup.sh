bbc1b4ebfdf9a6fdb3925c78ed801bd85cef763b
jimmy-sama <lungmuss@proton.me>
2025-09-14 21:39:00 +0200

    add some startup and ricing options

---
+++ roles/dwl/files/dwl/dwlStartup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Kill already running dublicate process
+_ps="waybar mako swaybg"
+for _prs in $_ps; do
+    if [ "$(pidof "${_prs}")" ]; then
+         killall -9 "${_prs}"
+    fi
+ done
+
+dunst &
+waybar &
+
