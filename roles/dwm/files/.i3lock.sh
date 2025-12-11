e90caa428d7804b3d97d072d8084adc3c7da245e
jimmy-sama <lungmuss@proton.me>
2025-12-11 17:55:42 +0100

    cleanup

--- roles/dwm/files/.i3lock.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-convert ~/Pictures/Wallpaper/localhostV6.png -resize $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/') RGB:- | i3lock --raw $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'):rgb --image /dev/stdin
